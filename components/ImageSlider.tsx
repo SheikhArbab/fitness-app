2aa90c3f36d4ee299c05c271e959dd74b2aa2a27
SheikhArbab <arbabzafar4444@gmail.com>
2024-09-02 01:56:48 +0500

    add images

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+import Carousel, { ParallaxImage } from 'react-native-snap-carousel'
 
 export default function ImageSlider() {
     return (
